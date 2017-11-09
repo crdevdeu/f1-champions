1d2647b48d5d7d5ccc447684dbaeb6f61575c553
Carlos Rincones <carlos.rincones@jobsity.com>
2017-11-09 14:24:43 -0500

    chore: add environment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiHost: 'http://ergast.com/api',
+  championsListOffset: '55',
+  championsListLimit: '10',
+  seasonResultsPageSizeOptionsOffset: 5
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,9 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiHost: 'http://ergast.com/api',
+  championsListOffset: 55,
+  championsListLimit: 11,
+  seasonResultsPageSizeOptionsOffset: 5
 };
